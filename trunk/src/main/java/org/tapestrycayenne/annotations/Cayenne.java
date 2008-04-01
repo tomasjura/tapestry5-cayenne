18922676082f389a795333c1e7095071df925330
nirvdrum <nirvdrum@f204f02f-ad48-0410-8b25-ed27bdd86450>
2008-04-01 09:42:06 +0000

    Minor cleanups.

--- trunk/src/main/java/org/tapestrycayenne/annotations/Cayenne.java
+++ trunk/src/main/java/org/tapestrycayenne/annotations/Cayenne.java
@@ -5,22 +5,20 @@
  */
 package org.tapestrycayenne.annotations;
 
+import java.lang.annotation.Documented;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.PARAMETER;
-
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Marker annotation for use with Cayenne-related services
- * @author robertz
+ * Marker annotation for use with Cayenne-related services.
  *
+ * @author Robert Zeigler
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({PARAMETER,FIELD})
 @Documented
 public @interface Cayenne {
-
 }
