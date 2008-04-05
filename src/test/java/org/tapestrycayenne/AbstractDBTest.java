6d68f09ccfa78ccd376ee94c37ae61b66ea97957
robert.zeigler <robert.zeigler@f204f02f-ad48-0410-8b25-ed27bdd86450>
2008-04-05 03:35:00 +0000

    Started filling in tests.

---
+++ src/test/java/org/tapestrycayenne/AbstractDBTest.java
@@ -0,0 +1,44 @@
+/*
+ * Created on Apr 3, 2008
+ * 
+ * 
+ */
+package org.tapestrycayenne;
+
+import javax.sql.DataSource;
+
+import junit.framework.Assert;
+
+import org.apache.cayenne.access.DataContext;
+import org.apache.cayenne.access.DataNode;
+import org.apache.cayenne.access.DbGenerator;
+import org.apache.cayenne.conf.Configuration;
+import org.apache.cayenne.conf.DefaultConfiguration;
+import org.apache.cayenne.dba.DbAdapter;
+import org.apache.cayenne.dba.hsqldb.HSQLDBAdapter;
+import org.apache.cayenne.map.DataMap;
+
+public abstract class AbstractDBTest extends Assert {
+    
+    
+    protected static void setupdb() throws Exception {
+        DefaultConfiguration c = new DefaultConfiguration("cayenne.xml");
+        Configuration.initializeSharedConfiguration(c);
+        DbAdapter adapt = HSQLDBAdapter.class.newInstance();
+        DataContext dc = DataContext.createDataContext();
+        for(Object obj : dc.getEntityResolver().getDataMaps()) {
+            DataMap map = (DataMap) obj;
+            System.out.println(map.getName());
+            DataNode node = dc.getParentDataDomain().lookupDataNode(map);
+            System.out.println(node.getName());
+            DataSource src = node.getDataSource();
+            DbGenerator dbgen = new DbGenerator(adapt,map);
+            dbgen.setShouldCreatePKSupport(true);
+            dbgen.setShouldCreateFKConstraints(true);
+            dbgen.setShouldCreateTables(true);
+            dbgen.runGenerator(src);
+        }
+        DataContext.bindThreadDataContext(dc);
+    }
+
+}
