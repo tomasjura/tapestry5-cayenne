3c9ec94bff1367c1cdc7a343a4243413eb6f0bcc
nirvdrum <nirvdrum@f204f02f-ad48-0410-8b25-ed27bdd86450>
2008-04-16 00:45:26 +0000

    Moving server clases out of core module and into server module.

---
+++ tapestry5-cayenne-server/src/main/java/org/tapestrycayenne/services/CayenneRequestFilter.java
@@ -0,0 +1,48 @@
+/*
+ * Created on Mar 18, 2008
+ * 
+ * 
+ */
+package org.tapestrycayenne.services;
+
+import java.io.IOException;
+
+import org.apache.cayenne.access.DataContext;
+import org.apache.tapestry.services.ApplicationStateManager;
+import org.apache.tapestry.services.Request;
+import org.apache.tapestry.services.RequestFilter;
+import org.apache.tapestry.services.RequestHandler;
+import org.apache.tapestry.services.Response;
+
+/**
+ * Provides a RequestFilter which ensures that there is a DataContext associated with the current request.
+ * Currently uses a session-based strategy.
+ * @author robertz
+ *
+ */
+public class CayenneRequestFilter implements RequestFilter {
+    
+    private final ApplicationStateManager _asm;
+    
+    public CayenneRequestFilter(final ApplicationStateManager asm) {
+        _asm = asm;
+    }
+
+    public boolean service(Request request, Response response, RequestHandler handler)
+            throws IOException {
+        DataContext dc=null;
+        if (_asm.exists(DataContext.class)) {
+            dc = _asm.get(DataContext.class);
+        } else {
+            dc = DataContext.createDataContext();
+            _asm.set(DataContext.class, dc);
+        }
+        DataContext.bindThreadDataContext(dc);
+        try {
+            return handler.service(request, response);
+        } finally {
+            DataContext.bindThreadDataContext(null);
+        }
+    }
+
+}
