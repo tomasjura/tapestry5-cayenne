100da16f3ce275f000588323595775cde9c11441
robert.zeigler <robert.zeigler@f204f02f-ad48-0410-8b25-ed27bdd86450>
2008-04-24 17:59:07 +0000

    Issue 2: CayenneEntityEncoder should handle null values Issue 3: CayenneEntityEncoder should handle transient and new objects Issue 5: CayenneEntityEncoder should handle non-integer primary keys
    Updates previous commit to strip LRUMap from CayenneEntityEncoder; that solution wouldn't work because tapestry can potentially create multiple encoders, since they are created via a factory.

--- tapestry5-cayenne-core/src/main/java/org/tapestrycayenne/services/CayenneEntityEncoder.java
+++ tapestry5-cayenne-core/src/main/java/org/tapestrycayenne/services/CayenneEntityEncoder.java
@@ -5,47 +5,105 @@
  */
 package org.tapestrycayenne.services;
 
-import org.apache.cayenne.DataObjectUtils;
-import org.apache.cayenne.Persistent;
-import org.apache.tapestry.ValueEncoder;
-
+import java.util.Collection;
 import java.util.regex.Pattern;
 
+import org.apache.cayenne.DataObjectUtils;
+import org.apache.cayenne.PersistenceState;
+import org.apache.cayenne.Persistent;
+import org.apache.cayenne.map.DbAttribute;
+import org.apache.cayenne.map.DbEntity;
+import org.apache.cayenne.map.ObjAttribute;
+import org.apache.cayenne.map.ObjEntity;
+import org.apache.tapestry.ValueEncoder;
+import org.apache.tapestry.ioc.services.TypeCoercer;
+
 /**
- * Basic CayenneDataObject ValueEncoder.
- * This works for objects which implements Persistent.  
- * If you're using the POJO facilities of cayenne, you will need to contribute your own value encoders.
- * This ValueEncoder also assumes that the primary key of your object is a single int column.
- *
+ * Basic Persistent ValueEncoder.
+ * This works for objects which implements Persistent. If you're using the POJO
+ * facilities of cayenne, you will need to contribute your own value encoders.
+ * This ValueEncoder assumes a single-column primary key. If your entity uses a
+ * multi-column key, you will need to contribute a custom ValueEncoder for that
+ * entity.  If the pk type is something other than "INTEGER", you must have
+ * an object attribute corresponding to the primary key's db attribute.
  * @author Robert Zeigler
  */
 public class CayenneEntityEncoder implements ValueEncoder<Persistent> {
     
     private final ObjectContextProvider _provider;
-    private Pattern _pattern = Pattern.compile("::");
+    private final Pattern _pattern = Pattern.compile("::");
+    private final TypeCoercer _coercer;
+    private final NonPersistedObjectStorer _storer;
     
-    public CayenneEntityEncoder(final ObjectContextProvider provider) {
+    @SuppressWarnings("unchecked")
+    public CayenneEntityEncoder(
+            final ObjectContextProvider provider,
+            final TypeCoercer coercer,
+            final NonPersistedObjectStorer storer) {
         _provider = provider;
+        _coercer = coercer;
+        _storer=storer;
     }
 
-    // TODO null handling
     public String toClient(final Persistent dao)
     {
-        return dao.getObjectId().getEntityName() + "::" + 
-            Integer.toString(DataObjectUtils.intPKForObject(dao));
+        if (dao == null) {
+            return "nil";
+        }
+        if (dao.getPersistenceState() == PersistenceState.NEW
+                || dao.getPersistenceState() == PersistenceState.TRANSIENT) {
+            String key = _storer.store(dao);
+            //TODO smells of tight coupling here, having to dig through so many layers of objects.
+            ObjEntity ent = _provider.currentContext().getEntityResolver().lookupObjEntity(dao.getClass());
+            return ent.getName() + "::t::" + key;
+            
+        }
+        String pk = _coercer.coerce(DataObjectUtils.pkForObject(dao),String.class);
+        return dao.getObjectId().getEntityName() + "::" + pk;
     }
 
-    // TODO null handling
     public Persistent toValue(final String val) {
+        if (val == null || val.trim() == "") { 
+            return null;
+        }
         String[] vals = _pattern.split(val);
-
-        if (vals.length != 2)
+        if (vals.length < 2)
         {
+            if (vals[0].equals("nil")) {
+                return null;
+            }
             //TODO i18n this
             throw new RuntimeException("Unable to convert " + val + " into a CayenneDataObject");
         }
-
+        if (vals.length == 3) {
+            //check to see if it's in storage...
+            Persistent obj = _storer.retrieve(vals[2]);
+            if (obj == null) { 
+                throw new RuntimeException("Unable to convert " + val + " into a CayenneDataObject: missing object");
+            }
+            return obj; 
+        }
+        
+        Object pk = _coercer.coerce(vals[1], pkTypeForEntity(vals[0]));
         return (Persistent) 
-            DataObjectUtils.objectForPK(_provider.currentContext(), vals[0], Integer.parseInt(vals[1]));
+            DataObjectUtils.objectForPK(_provider.currentContext(), vals[0], pk);
+    }
+    
+    private Class<?> pkTypeForEntity(String entity) {
+        ObjEntity oent = _provider.currentContext().getEntityResolver().getObjEntity(entity);
+        DbEntity dbent = oent.getDbEntity();
+        Collection<DbAttribute> atts = dbent.getPrimaryKeys();
+        if (atts.size() > 1) {
+            throw new RuntimeException("CayenneEntityEncoder can't handle multi-column pks");
+        }
+        DbAttribute dbatt = atts.iterator().next();
+        ObjAttribute attribute = oent.getAttributeForDbAttribute(dbatt);
+        if (attribute == null) {
+            if (dbatt.getType() != java.sql.Types.INTEGER) {
+                throw new RuntimeException("Primary Key types other than INTEGER must have a corresponding object property");
+            }
+            return Integer.class;
+        }
+        return attribute.getJavaClass();
     }
 }
