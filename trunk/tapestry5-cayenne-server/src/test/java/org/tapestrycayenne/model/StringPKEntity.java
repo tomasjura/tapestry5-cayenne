a7f9896aa6a70053447f7efcf44708d22ef7d43d
robert.zeigler <robert.zeigler@f204f02f-ad48-0410-8b25-ed27bdd86450>
2008-04-24 17:03:10 +0000

    Fix for issue 2: CayenneEntityEncoder should handle null values Fix for issue 3: CayenneEntityEncoder should handle transient and new objects Fix for issue 5: CayenneEntityEncoder should handle non-integer primary keys

---
+++ trunk/tapestry5-cayenne-server/src/test/java/org/tapestrycayenne/model/StringPKEntity.java
@@ -0,0 +1,9 @@
+package org.tapestrycayenne.model;
+
+import org.tapestrycayenne.model.auto._StringPKEntity;
+
+
+
+public class StringPKEntity extends _StringPKEntity {
+
+}
--- trunk/tapestry5-cayenne-server/src/test/java/org/tapestrycayenne/services/TestCayenneEntityEncoder.java
+++ trunk/tapestry5-cayenne-server/src/test/java/org/tapestrycayenne/services/TestCayenneEntityEncoder.java
@@ -7,8 +7,12 @@ package org.tapestrycayenne.services;
 
 import org.apache.cayenne.DataObjectUtils;
 import org.apache.cayenne.Persistent;
+import org.apache.tapestry.ioc.Registry;
+import org.apache.tapestry.ioc.services.TypeCoercer;
 import org.tapestrycayenne.AbstractDBTest;
 import org.tapestrycayenne.model.Artist;
+import org.tapestrycayenne.model.StringPKEntity;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -18,33 +22,55 @@ public class TestCayenneEntityEncoder extends AbstractDBTest {
     
     private CayenneEntityEncoder _encoder;
     private ObjectContextProvider _provider;
+    private Registry _registry;
 
     @BeforeTest
     void setupDB() throws Exception {
         AbstractDBTest.setupdb();
-        _provider = new DataContextProviderImpl();
-        _encoder = new CayenneEntityEncoder(_provider);
+        _registry = AbstractDBTest.setupRegistry("App0",TapestryCayenneModule.class);
+        _provider = _registry.getService(ObjectContextProvider.class);
+        _encoder = new CayenneEntityEncoder(_provider,_registry.getService(TypeCoercer.class));
     }
     
+    @AfterTest
+    void shutdownRegistry() {
+        if (_registry != null) {
+            _registry.shutdown();
+        }
+    }
+    
+    @SuppressWarnings("unused")
     @DataProvider(name="conversions")
     private Object[][] conversionValues() {
         Artist a = _provider.currentContext().newObject(Artist.class);
         a.setName("test");
+        StringPKEntity spke = _provider.currentContext().newObject(StringPKEntity.class);
+        spke.setId("testingstrings");
         _provider.currentContext().commitChanges();
+        Artist a2 = new Artist();
+        Artist a3 = _provider.currentContext().newObject(Artist.class);
+        
         return new Object[][] {
-                //TODO add test (and appropriate code) for null handling
-                //TODO add test (and appropriate code) for transient object handling
-                //TODO add test (and appropriate code) for new object handling
-                //TODO might be nice to have a "secure" way of storing objects in the url.
+                //Null object handling.
+                {null,"nil"},
+                //transient object handling
+                {a2,"Artist::" + "h" + a2.hashCode()},
+                //"new" object handling.
+                {a3, "Artist::" + "h" + a3.hashCode()},
+                //committed object handling, int pk.
                 {a,"Artist::" + DataObjectUtils.intPKForObject(a)},
+                //committed object handling non-numeric pk
+                {spke,"StringPKEntity::testingstrings"}
+                //TODO might be nice to have a way to store objs in the url in a "tamper-proof" fashion.
+                //at least as an option.
         };
     }
     
     @Test(dataProvider="conversions")
     void testConversion(Persistent serverVal, String clientVal) {
         String client = _encoder.toClient(serverVal);
-        assertEquals("Encoder incorrectly encoded artist value",client,clientVal);
+        assertEquals(client,clientVal,"Encoder incorrectly encoded artist value");
         Persistent server = _encoder.toValue(client);
-        assertEquals("Encoder incorrectly converted client value to server value",server,serverVal);
+        assertEquals(server,serverVal,"Encoder incorrectly converted client value to server value");
     }
 }
