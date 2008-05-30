41d06b6ef31bde6534dea7f0d3fe300ca084a027
robert.zeigler <robert.zeigler@f204f02f-ad48-0410-8b25-ed27bdd86450>
2008-05-30 18:51:24 +0000

    1) Add a "to one" viewer 2) Make it so that contributions to DefaultDataTypeAnalyzer are respected by CayenneDataTypeAnalyzer 3) More tests

--- trunk/tapestry5-cayenne-server/src/test/java/org/tapestrycayenne/integration/app0/pages/TestToOneControl.java
+++ trunk/tapestry5-cayenne-server/src/test/java/org/tapestrycayenne/integration/app0/pages/TestToOneControl.java
@@ -1,11 +1,14 @@
 package org.tapestrycayenne.integration.app0.pages;
 
 import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.corelib.components.BeanDisplay;
 import org.apache.tapestry5.corelib.components.BeanEditForm;
 import org.tapestrycayenne.model.Painting;
 
 public class TestToOneControl {
     
+    @Persist
     private Painting _painting;
     
     @SuppressWarnings("unused")
@@ -13,6 +16,14 @@ public class TestToOneControl {
             "object=painting"
     })
     private BeanEditForm _form;
+    
+    @SuppressWarnings("unused")
+    @Component(parameters={
+           "object=painting" 
+    })
+    private BeanDisplay _display;
+    
+    
     
     public Painting getPainting() {
         return _painting;
