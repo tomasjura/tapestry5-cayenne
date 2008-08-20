ebc014e5f99e9b038e46f3e87d79b3c0c67e9f24
nirvdrum <nirvdrum@f204f02f-ad48-0410-8b25-ed27bdd86450>
2008-08-20 01:30:01 +0000

    Moved RequestFilter code to tapestry-core.

--- trunk/tapestry5-cayenne-server/src/main/java/com/googlecode/tapestry5cayenne/services/TapestryCayenneModule.java
+++ trunk/tapestry5-cayenne-server/src/main/java/com/googlecode/tapestry5cayenne/services/TapestryCayenneModule.java
@@ -6,17 +6,11 @@
 package com.googlecode.tapestry5cayenne.services;
 
 import org.apache.tapestry5.ioc.Configuration;
-import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.SubModule;
-import org.apache.tapestry5.ioc.annotations.Symbol;
 import org.apache.tapestry5.services.AliasContribution;
-import org.apache.tapestry5.services.RequestFilter;
 
 import com.googlecode.tapestry5cayenne.annotations.Cayenne;
-import com.googlecode.tapestry5cayenne.services.NonPersistedObjectStorer;
-import com.googlecode.tapestry5cayenne.services.ObjectContextProvider;
-import com.googlecode.tapestry5cayenne.services.TapestryCayenneCoreModule;
 
 @SubModule(TapestryCayenneCoreModule.class)
 public class TapestryCayenneModule {
@@ -26,19 +20,6 @@ public class TapestryCayenneModule {
     {
         binder.bind(ObjectContextProvider.class, DataContextProviderImpl.class)
             .withMarker(Cayenne.class).withId("DataContext");
-
-        binder.bind(RequestFilter.class, CayenneRequestFilter.class)
-            .withId("CayenneFilter")
-            .withMarker(Cayenne.class);
-    }
-    
-    public static void contributeRequestHandler(OrderedConfiguration<RequestFilter> configuration,
-            @Cayenne 
-            RequestFilter filter,
-            @Symbol(TapestryCayenneCoreModule.FILTER_LOCATION)  
-            String location)
-    {
-        configuration.add("cayenne", filter, location);
     }
     
     public static void contributeAlias(Configuration<AliasContribution> conf,
@@ -47,5 +28,4 @@ public class TapestryCayenneModule {
         conf.add(AliasContribution.create(NonPersistedObjectStorer.class, storer));
         conf.add(AliasContribution.create(ObjectContextProvider.class, provider));
     }
-
 }
