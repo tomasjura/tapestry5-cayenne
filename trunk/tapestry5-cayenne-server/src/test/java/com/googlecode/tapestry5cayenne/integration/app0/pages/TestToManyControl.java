279a7ea7427abf421c8981263157764a4cd857c3
robert.zeigler <robert.zeigler@f204f02f-ad48-0410-8b25-ed27bdd86450>
2009-03-28 06:55:22 +0000

    Fix broken tests. They broke as a result of modifying the test app model for the new validation constrain tests.

--- trunk/tapestry5-cayenne-server/src/test/java/com/googlecode/tapestry5cayenne/integration/app0/pages/TestToManyControl.java
+++ trunk/tapestry5-cayenne-server/src/test/java/com/googlecode/tapestry5cayenne/integration/app0/pages/TestToManyControl.java
@@ -7,9 +7,12 @@ import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.beaneditor.BeanModel;
 import org.apache.tapestry5.corelib.components.BeanDisplay;
 import org.apache.tapestry5.corelib.components.Grid;
+import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.BeanModelSource;
 
 import com.googlecode.tapestry5cayenne.model.Artist;
 import com.googlecode.tapestry5cayenne.services.ObjectContextProvider;
@@ -28,14 +31,27 @@ public class TestToManyControl {
     private List<Artist> _artistList;
     
     @Component(parameters={
-            "object=artist"
+            "object=artist","model=artistModel"
     })
     @SuppressWarnings("unused")
     private BeanDisplay _display;
     
     @SuppressWarnings("unused")
-    @Component(parameters={"source=artistList"})
+    @Component(parameters={"source=artistList","model=artistModel"})
     private Grid _gridDisplay;
+    
+    
+    @Inject
+    private BeanModelSource source;
+    
+    @Inject
+    private Messages messages;
+    
+    public BeanModel<Artist> getArtistModel() {
+        BeanModel<Artist> ret = source.createDisplayModel(Artist.class, messages);
+        ret.include(Artist.PAINTING_LIST_PROPERTY,Artist.PAINTINGS_BY_TITLE_PROPERTY);
+        return ret;
+    }
     
     @SetupRender
     @SuppressWarnings("unchecked")
