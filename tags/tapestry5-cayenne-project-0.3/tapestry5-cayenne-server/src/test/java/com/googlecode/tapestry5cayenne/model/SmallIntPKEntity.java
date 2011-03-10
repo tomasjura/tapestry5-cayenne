0914c4654099ad70b7fffdced53118af319b1325
robert.zeigler <robert.zeigler@f204f02f-ad48-0410-8b25-ed27bdd86450>
2011-03-10 05:38:42 +0000

    Remove tag for failed 0.3 release.

--- tags/tapestry5-cayenne-project-0.3/tapestry5-cayenne-server/src/test/java/com/googlecode/tapestry5cayenne/model/SmallIntPKEntity.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.tapestry5cayenne.model;
-
-import com.googlecode.tapestry5cayenne.model.auto._SmallIntPKEntity;
-
-@SuppressWarnings("serial")
-public class SmallIntPKEntity extends _SmallIntPKEntity {
-
-}
