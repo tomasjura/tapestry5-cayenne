94dca9b3e593f55a99d82a7a1a4720b8a14198b8
robert.zeigler <robert.zeigler@f204f02f-ad48-0410-8b25-ed27bdd86450>
2011-07-15 06:20:24 +0000

    Remove tag for borked build.

--- tags/tapestry5-cayenne-project-0.4/tapestry5-cayenne-server/src/test/java/com/googlecode/tapestry5cayenne/model/AcceptedBid.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.tapestry5cayenne.model;
-
-import com.googlecode.tapestry5cayenne.model.auto._AcceptedBid;
-
-public class AcceptedBid extends _AcceptedBid {
-
-}
--- tags/tapestry5-cayenne-project-0.4/tapestry5-cayenne-server/src/test/java/com/googlecode/tapestry5cayenne/model/BigIntPKEntity.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.tapestry5cayenne.model;
-
-import com.googlecode.tapestry5cayenne.model.auto._BigIntPKEntity;
-
-@SuppressWarnings("serial")
-public class BigIntPKEntity extends _BigIntPKEntity {
-
-}
