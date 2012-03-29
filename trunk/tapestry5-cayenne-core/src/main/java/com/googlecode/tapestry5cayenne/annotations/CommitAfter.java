8e186e3b7d9f9e413f6ceb079a6c6c4e6660a97a
robert.zeigler <robert.zeigler@f204f02f-ad48-0410-8b25-ed27bdd86450>
2012-03-29 12:33:48 +0000

    Fix problem with CommitAfter and fix integration test name to be picked up by maven.

--- trunk/tapestry5-cayenne-core/src/main/java/com/googlecode/tapestry5cayenne/annotations/CommitAfter.java
+++ trunk/tapestry5-cayenne-core/src/main/java/com/googlecode/tapestry5cayenne/annotations/CommitAfter.java
@@ -1,5 +1,11 @@
 package com.googlecode.tapestry5cayenne.annotations;
 
+import java.lang.annotation.Documented;
+import static java.lang.annotation.ElementType.METHOD;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Perfectly analogous to the tapestry-hibernate "CommitAfter" annotation.
  * Apply this to a method where you want the current object context to be committed.
@@ -8,6 +14,10 @@ package com.googlecode.tapestry5cayenne.annotations;
  * Generally, you should handle committing yourself to ensure that you handle errors in a meaningful way.
  * However, this annotation is useful for rapid prototyping.
  */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({METHOD})
+@Documented
 public @interface CommitAfter {
 
 }
